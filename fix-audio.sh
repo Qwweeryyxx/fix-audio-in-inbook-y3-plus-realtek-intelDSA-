6c8d187dea29ae06f42596663550268200043de2
Qwweeryyxx <155798483+Qwweeryyxx@users.noreply.github.com>
2026-04-12 00:43:21 +0500

    Refactor audio fix script for Infinix Inbook Y3 Plus
    Updated audio fix script for Infinix Inbook Y3 Plus, including configuration for microphone and speaker settings.

--- fix-audio.sh
+++ fix-audio.sh
@@ -8,13 +8,37 @@ fi
 
 echo "Start fix"
 
-sudo tee /etc/modprobe.d/alsa-base.conf << EOF
+#!/bin/bash
+echo "=== ФИКС ЗВУКА И МИКРОФОНА Infinix Inbook Y3 Plus ==="
+
+sudo tee /etc/modprobe.d/fix-infinix-alc269.conf > /dev/null <<EOF
 options snd-hda-intel model=dell-inspiron-7559
+options snd-hda-intel power_save=0
+options snd-hda-intel power_save_controller=N
 EOF
 
-sudo alsa force-reload
+# reboot driver
+sudo modprobe -r snd_hda_intel 2>/dev/null || true
+sudo modprobe snd_hda_intel
 
-echo "fix finish!"
-echo "auto-reboot the system after 15 seconds, press Ctrl+C to cancel"
+sleep 2
+
+
+amixer -c 0 set 'Master' 80% unmute
+amixer -c 0 set 'Speaker' 100% unmute
+amixer -c 0 set 'PCM' 90% unmute
+
+
+amixer -c 0 set 'Internal Mic' 100% unmute
+amixer -c 0 set 'Internal Mic Boost' 0
+amixer -c 0 set 'Capture' 66% unmute
+amixer -c 0 set 'Digital' 24% unmute
+
+# Save
+sudo alsactl store
+
+echo "✅ Done!"
+echo "микрофон теперь работает ништяк и звук хотябы пашет.\n если работа микрофона не так как вы хотите, пропишите alsamixer,\n нажмите F6, выберите вариант с написью intel,\n затем F4, отрегулируй Capture (это громкость микрофона) по своему желанию\n а зачем нажми Ctrl + C и пропиши sudo alsactl store"
+echo "щас ноут перезагрузит через 15 секунд, если не хочешь - Ctrl + C"
 sleep 15
-reboot
+sudo reboot
